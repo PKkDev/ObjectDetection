1e29f6f1976ad223479ecbc25483b291141a86df
Kirill Portnov <prodevkir@mail.ru>
2023-02-25 23:03:54 +0400

    update

--- ObjectDetection.WinApp/MainWindow.xaml.cs
+++ ObjectDetection.WinApp/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
 using Microsoft.UI.Xaml;
+using System;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Enumeration;
+using Windows.Devices.PointOfService;
 
 namespace ObjectDetection.WinApp
 {
@@ -7,6 +12,37 @@ namespace ObjectDetection.WinApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async Task Open()
+        {
+            DevicePicker devicePicker = new();
+            devicePicker.Filter.SupportedDeviceClasses.Add(DeviceClass.All);
+
+            //BluetoothLEDevice
+            //devicePicker.Filter.SupportedDeviceSelectors.Add();
+
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(App.MainWindow);
+            WinRT.Interop.InitializeWithWindow.Initialize(devicePicker, hwnd);
+
+            Windows.Foundation.Rect rect = new Windows.Foundation.Rect(new Windows.Foundation.Point(0, 0), new Windows.Foundation.Point(0, 0));
+
+            DeviceInformation di = await devicePicker.PickSingleDeviceAsync(rect);
+            if (di != null)
+            {
+                try
+                {
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await Open();
         }
     }
 }
